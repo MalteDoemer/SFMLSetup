8fa3610d2ee7692a1f7cb79d32a15d59cec839a4
Malte Dömer <maltedoemer@gmail.com>
2020-07-24 09:22:36 +0200

    removed unnesecary pragma comments

--- srcfiles/sfml.h
+++ srcfiles/sfml.h
@@ -18,14 +18,15 @@
 
 #pragma comment(lib, "kernel32.lib")
 #pragma comment(lib, "user32.lib")
-#pragma comment(lib, "winspool.lib")
-#pragma comment(lib, "comdlg32.lib")
 #pragma comment(lib, "advapi32.lib")
-#pragma comment(lib, "shell32.lib")
-#pragma comment(lib, "ole32.lib")
-#pragma comment(lib, "oleaut32.lib")
-#pragma comment(lib, "uuid.lib")
-#pragma comment(lib, "odbc32.lib")
-#pragma comment(lib, "odbccp32.lib")
+
+//#pragma comment(lib, "winspool.lib")
+//#pragma comment(lib, "comdlg32.lib")
+//#pragma comment(lib, "shell32.lib")
+//#pragma comment(lib, "ole32.lib")
+//#pragma comment(lib, "oleaut32.lib")
+//#pragma comment(lib, "uuid.lib")
+//#pragma comment(lib, "odbc32.lib")
+//#pragma comment(lib, "odbccp32.lib")
 
 #include <SFML/Graphics.hpp>
